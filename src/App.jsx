1125ebf0d9a5e8b511e303f0327e610d259b64e6
yanghyeyun <124745402+yanghyeyun@users.noreply.github.com>
2023-02-13 10:17:43 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,8 @@ function App() {
   const [isLong, setIsLong] = useState(true); //eslint-disable-line no-unused-vars
   const [leverage, setLeverage] = useState(1); //eslint-disable-line no-unused-vars
   const [result, setResult] = useState(
-        (((((bidPrice*maxPrice)-(((askPrice*maxPrice)*0.00015)+((bidPrice*maxPrice)*0.00015)+((bidPrice * maxPrice)*0.003)))-(askPrice*maxPrice))/(1000*100))*100).toFixed(2)
-  ); //eslint-disable-line no-unused-vars
-
+        Number(((((bidPrice*maxPrice)-(((askPrice*maxPrice)*0.00015)+((bidPrice*maxPrice)*0.00015)+((bidPrice * maxPrice)*0.003)))-(askPrice*maxPrice))/(askPrice*maxPrice))*100).toFixed(2)
+  );
   const [tangu, setTangu] = useState(false);
 
    useEffect(() => {
@@ -148,7 +147,7 @@ function App() {
 
               }}
             >
-              +{Number(((((bidPrice*maxPrice)-(((askPrice*maxPrice)*0.00015)+((bidPrice*maxPrice)*0.00015)+((bidPrice * maxPrice)*0.003)))-(askPrice*maxPrice))/(askPrice*maxPrice))*100).toFixed(2)}%
+              +{result}%
 
             </div>
           </div> 
